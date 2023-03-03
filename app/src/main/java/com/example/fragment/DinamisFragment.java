24dbd717dbbbba763ee8ecc585f56570e1a39776
farhanakmals <shaleh.farhan@gmail.com>
2023-03-03 22:44:15 +0700

    second commit

--- app/src/main/java/com/example/fragment/DinamisFragment.java
+++ app/src/main/java/com/example/fragment/DinamisFragment.java
@@ -10,23 +10,23 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link SimpleFragment} factory method to
+ * Use the {@link DinamisFragment} factory method to
  * create an instance of this fragment.
  */
-public class SimpleFragment extends Fragment {
+public class DinamisFragment extends Fragment {
 
-    public SimpleFragment() {
+    public DinamisFragment() {
         // Required empty public constructor
     }
 
-    public static SimpleFragment newInstance() {
-        return new SimpleFragment();
+    public static DinamisFragment newInstance() {
+        return new DinamisFragment();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_simple, container, false);
+        return inflater.inflate(R.layout.fragment_dinamis, container, false);
     }
 }
--- app/src/main/java/com/example/fragment/MainActivity.java
+++ app/src/main/java/com/example/fragment/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
+    private boolean isFragmentDisplayed = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mButton = findViewById(R.id.btnNext);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment;
+                if (!isFragmentDisplayed) {
+                    fragment = SimpleFragment.newInstance();
+                    mButton.setText(R.string.btn1);
+                } else {
+                    fragment = DinamisFragment.newInstance();
+                    mButton.setText(R.string.btn2);
+                }
+                displayFragment(fragment);
+                isFragmentDisplayed = !isFragmentDisplayed;
+            }
+        });
+
+        displayFragment(SimpleFragment.newInstance());
+    }
+
+    private void displayFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager
+                .beginTransaction();
+
+        fragmentTransaction.replace(R.id.fragment2, fragment)
+                .addToBackStack(null)
+                .commit();
     }
 }
